42cf9fd121d6af5533af3ce0fa0f896fac043a0d
amirali-safaie <amirali.safie@gmail.com>
2022-12-28 20:52:41 +0330

    add is_author and special user in admin panel using useradmin

--- first_projecct/account/admin.py
+++ first_projecct/account/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+UserAdmin.fieldsets[2][1]["fields"]=(     #hاضافه کردن فیلد های خودم به پنل ادمین
+                                     "is_active",
+                                     "is_staff",
+                                     "is_superuser",
+                                     "is_author",
+                                     "special_user",
+                                     "groups",
+                                     "user_permissions",
+                                     )
+
+UserAdmin.list_display += ("is_author","special_or_not")
+
 admin.site.register(User, UserAdmin)
+
+
 
 # Register your models here.
--- first_projecct/account/models.py
+++ first_projecct/account/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser #کلاس یوزر خود جنگو که قرار ما توش ی سری فیلد اضافه کنیم
 
-class User(AbstractUser):
+class User(AbstractUser):   #ارث بری از کلاس یوزر خود جنگو و ایجاد یوزر جدید برای استفاده توی پروژه
     is_author = models.BooleanField(default=False)
-    special_user= models.DateTimeField(default=timezone.now)
+    special_user = models.DateTimeField(default=timezone.now)
 
     def special_or_not(self):
-        if(timezone.now<self.special_user):
+        if(timezone.now() < self.special_user):   #ایا یوزر الان یوزر طلایی هست یا ن
             return True
         else:
             return False
-
+    special_or_not.boolean = True
+    special_or_not.short_description = "special user"
 
 
 # Create your models here.
