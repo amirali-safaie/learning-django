53dec4aaf82831686b0ac3a600b6f29fcedf6785
amirali-safaie <63147450+amirali-safaie@users.noreply.github.com>
2023-01-17 09:10:07 -0500

    make active bar with template tags

--- first_projecct/blog/templatetags/base_tag.py
+++ first_projecct/blog/templatetags/base_tag.py
@@ -8,3 +8,13 @@ register = template.Library()
 def category_navbar():
     return {"category":Category.objects.filter(status = True)}
 
+@register.inclusion_tag("registration/partial/active_bar.html") #این تگ برای اکتیو باره لینک و اسم و اون تیکه رو میگیره و یک منو درست میکنه
+def active_bar(request,link_name,content):
+    return {
+        "request":request,
+        "link_name":link_name, #لینکی که میخوام بهش برم 
+        "link":f'account:{link_name}',
+        "content":content
+    }
+
+
