0d10a53bdb0f30e340c389127a1b5e614e405888
amirali-safaie <63147450+amirali-safaie@users.noreply.github.com>
2023-02-06 08:05:18 -0500

    do something to activate account using email but its still not complete

--- first_projecct/account/forms.py
+++ first_projecct/account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm  
+  
 
 class ProfileForm(forms.ModelForm):
     def __init__(self,*args,**kwargs):
@@ -12,3 +14,11 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username","email","first_name","last_name","special_user","is_author"]
+
+
+
+class SignupForm(UserCreationForm):  
+    email = forms.EmailField(max_length=200, help_text='Required')  
+    class Meta:  
+        model = User  
+        fields = ('username', 'email', 'password1', 'password2')  
--- first_projecct/account/views.py
+++ first_projecct/account/views.py
@@ -1,5 +1,15 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse  
+from django.shortcuts import render, redirect  
+from django.http import HttpResponse  
+from .forms import SignupForm  
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_text  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .tokens import account_activation_token  
+from django.core.mail import EmailMessage  
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView,PasswordChangeView
@@ -163,3 +173,69 @@ class ChangePassword(PasswordChangeView):
 
     success_url = reverse_lazy("account:make_profile")
     template_name = "registration/change_pass.html"
+
+
+
+
+
+
+
+
+class Register(CreateView):
+    form_class = SignupForm
+    template_name = "registration/register.html"
+    def form_valid(self, form):
+            # save form in the memory not in database  
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()  
+            # to get the domain of the current site  
+            current_site = get_current_site(self.request)  
+            mail_subject = 'Activation link has been sent to your email id'  
+            message = render_to_string('registration/activate_account.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                        mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')  
+
+
+
+
+
+
+
+
+  
+def signup(request):  
+    if request.method == 'POST':  
+        form = SignupForm(request.POST)  
+        if form.is_valid():  
+            # save form in the memory not in database  
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()  
+            # to get the domain of the current site  
+            current_site = get_current_site(request)  
+            mail_subject = 'Activation link has been sent to your email id'  
+            message = render_to_string('acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                        mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')  
+    else:  
+        form = SignupForm()  
+    return render(request, 'signup.html', {'form': form})  
